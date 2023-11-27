e1e692bc4bc038d0aa6c87a932a71e0f4c437e30
Hammediora <128177086+Hammediora@users.noreply.github.com>
2023-11-26 21:21:43 -0600

    i just commit these

--- homePage.js
+++ homePage.js
@@ -1,29 +1,50 @@
+// Scroll to Top Button Functionality
 window.addEventListener('scroll', function() {
     const scrollBtn = document.getElementById('scrollBtn');
-
-    if (window.pageYOffset > 200) {
-        scrollBtn.style.display = 'block';  // or 'inline' or 'inline-block' depending on your design
-    } else {
-        scrollBtn.style.display = 'none';
-    }
+    scrollBtn.style.display = window.pageYOffset > 200 ? 'block' : 'none';
 });
 
 document.getElementById('scrollBtn').addEventListener('click', function(event) {
     event.preventDefault();
-
-    window.scrollTo({
-        top: 0,
-        behavior: 'smooth'
-    });
+    window.scrollTo({ top: 0, behavior: 'smooth' });
 });
 
+// Common functionality for closing modals
+window.addEventListener('click', function(event) {
+    if (event.target.classList.contains('modal')) {
+        event.target.style.display = 'none';
+    }
+});
 
+// Common functionality for Escape key
+window.addEventListener('keydown', function(event) {
+    if (event.key === 'Escape') {
+        document.querySelectorAll('.modal').forEach(function(modal) {
+            modal.style.display = 'none';
+        });
+    }
+});
 
-// Get the modal, button, and span elements
-    const modal = document.getElementById("gameModal");
-    const span = document.getElementsByClassName("close")[0];
-    const closeSpan = document.querySelector(".close");
+// Function to open a modal
+function openModal(modalId) {
+    const modal = document.getElementById(modalId);
+    if (modal) {
+        modal.style.display = 'block';
+    }
+}
 
+// Function to close a modal
+function closeModal(modalId) {
+    const modal = document.getElementById(modalId);
+    if (modal) {
+        modal.style.display = 'none';
+    }
+}
+
+// Initialize the game
+let currentAnswer = ""; // To store the current correct answer
+
+function startGame() {
     const codeSnippets = [{
         snippet: `
 public static void main(String[] args) {
@@ -76,49 +97,31 @@ function greet(name) {
             name: "JavaScript"
         }
     ];
+    let randomIndex = Math.floor(Math.random() * codeSnippets.length);
+    let selectedSnippet = codeSnippets[randomIndex];
 
-    // When the close button (x) is clicked, close the modal
-    span.onclick = function() {
-        modal.style.display = "none";
-    }
+    document.getElementById('code-snippet').textContent = selectedSnippet.snippet;
+    document.getElementById('game-result').textContent = "";
+    currentAnswer = selectedSnippet.answer;
+}
 
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function(event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
-    }
+function checkAnswer(userGuess) {
+    document.getElementById('game-result').textContent = userGuess === currentAnswer ? "Correct!" : "Wrong! Try Again.";
+}
 
-    // Add event listener for the close button
-    closeSpan.addEventListener('click', closeModal);
+// Event listeners for buttons
+document.getElementById("contactBtn").addEventListener('click', function() {
+    openModal('contactModal');
+});
 
-    function closeModal() {
-        modal.style.display = "none";
-    }
+document.getElementById("openGameModal").addEventListener('click', function() {
+    openModal('gameModal');
+    startGame();
+});
 
-    const openGameButton = document.getElementById("openGameModal");
-    openGameButton.addEventListener('click', openGameModal);
-
-    function openGameModal() {
-        modal.style.display = "block";
-        startGame();
-    }
-
-    function startGame() {
-        let randomIndex = Math.floor(Math.random() * codeSnippets.length);
-        let selectedSnippet = codeSnippets[randomIndex];
-
-        document.getElementById('code-snippet').textContent = selectedSnippet.snippet;
-        document.getElementById('game-result').textContent = ""; // Reset result from previous game
-        currentAnswer = selectedSnippet.answer; // Store the current answer to compare later
-    }
-
-    let currentAnswer = ""; // To store the current correct answer
-
-    function checkAnswer(userGuess) {
-        if (userGuess === currentAnswer) {
-            document.getElementById('game-result').textContent = "Correct!";
-        } else {
-            document.getElementById('game-result').textContent = "Wrong! Try Again.";
-        }
-    }
+// Close modals when the close button is clicked
+document.querySelectorAll('.modal .close').forEach(function(closeBtn) {
+    closeBtn.addEventListener('click', function() {
+        this.closest('.modal').style.display = 'none';
+    });
+});
